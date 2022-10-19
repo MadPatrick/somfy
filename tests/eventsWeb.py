46368e664f1fa7f27498b69e7b8752325b1ca879
janjaapko <janjaap.kostelijk@gmail.com>
2022-10-19 22:08:11 +0200

    finish unit tests

---
+++ tests/eventsWeb.py
@@ -0,0 +1,47 @@
+eventsWeb = [
+	{
+		"timestamp": 1666136420884,
+		"setupOID": "3cb5d24a-65df-4573-a86f-89ae87e5fc37",
+		"deviceURL": "io://0000-0000-0000/14287010",
+		"deviceStates": [
+			{
+				"name": "core:TargetClosureState",
+				"type": 1,
+				"value": "4"
+			},
+			{
+				"name": "core:MovingState",
+				"type": 6,
+				"value": "true"
+			}
+		],
+		"name": "DeviceStateChangedEvent"
+	},
+	{
+		"timestamp": 1666136421388,
+		"setupOID": "3cb5d24a-65df-4573-a86f-89ae87e5fc37",
+		"deviceURL": "io://0000-0000-0000/14287010",
+		"deviceStates": [
+			{
+				"name": "core:ManufacturerSettingsState",
+				"type": 0
+			},
+			{
+				"name": "core:ClosureState",
+				"type": 1,
+				"value": "98"
+			},
+			{
+				"name": "core:OpenClosedState",
+				"type": 3,
+				"value": "open"
+			},
+			{
+				"name": "core:TargetClosureState",
+				"type": 1,
+				"value": "5"
+			}
+		],
+		"name": "DeviceStateChangedEvent"
+	}
+]
