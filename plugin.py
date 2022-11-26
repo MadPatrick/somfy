76f69b8c40b9f5bd87188be5f85a485bb84b3d60
janjaapko <janjaap.kostelijk@gmail.com>
2022-11-26 19:47:10 +0100

    force relogin on failed command

--- plugin.py
+++ plugin.py
@@ -5,10 +5,10 @@
 # FirstFree function courtesy of @moroen https://github.com/moroen/IKEA-Tradfri-plugin
 # All credits for the plugin are for Nonolk, who is the origin plugin creator
 """
-<plugin key="tahomaIO" name="Somfy Tahoma or Connexoon plugin" author="MadPatrick" version="4.1.12" externallink="https://github.com/MadPatrick/somfy">
+<plugin key="tahomaIO" name="Somfy Tahoma or Connexoon plugin" author="MadPatrick" version="4.1.13" externallink="https://github.com/MadPatrick/somfy">
     <description>
 	<br/><h2>Somfy Tahoma/Connexoon plugin</h2><br/>
-        version: 4.1.12
+        version: 4.1.13
         <br/>This plugin connects to the Tahoma or Connexoon box either via the web API or via local access.
         <br/>Various devices are supported(RollerShutter, LightSensor, Screen, Awning, Window, VenetianBlind, etc.).
         <br/>For new devices, please raise a ticket at the Github link above.
@@ -271,7 +271,10 @@ class BasePlugin:
             return False
         if event_list is not None and len(event_list) > 0:
             self.update_devices_status(event_list)
-        self.heartbeat = False
+            self.heartbeat = False
+        if self.tahoma.logged_in == False:
+            #in case the send_command detected an error due to not logged in: force a heartbeat to get logged in again
+            self.heartbeat = True
         self.actions_serialized = []
         return True
 
