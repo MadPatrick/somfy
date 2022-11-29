d505e7a898fdf2c69f0ea33c75bcf1feb47bfeb1
janjaapko <janjaap.kostelijk@gmail.com>
2022-11-29 22:47:16 +0100

    improve rts devices

--- plugin.py
+++ plugin.py
@@ -5,10 +5,10 @@
 # FirstFree function courtesy of @moroen https://github.com/moroen/IKEA-Tradfri-plugin
 # All credits for the plugin are for Nonolk, who is the origin plugin creator
 """
-<plugin key="tahomaIO" name="Somfy Tahoma or Connexoon plugin" author="MadPatrick" version="4.1.16" externallink="https://github.com/MadPatrick/somfy">
+<plugin key="tahomaIO" name="Somfy Tahoma or Connexoon plugin" author="MadPatrick" version="4.1.17" externallink="https://github.com/MadPatrick/somfy">
     <description>
 	<br/><h2>Somfy Tahoma/Connexoon plugin</h2><br/>
-        version: 4.1.16
+        version: 4.1.17
         <br/>This plugin connects to the Tahoma or Connexoon box either via the web API or via local access.
         <br/>Various devices are supported(RollerShutter, LightSensor, Screen, Awning, Window, VenetianBlind, etc.).
         <br/>For new devices, please raise a ticket at the Github link above.
@@ -500,6 +500,9 @@ class BasePlugin:
                             swtype = 12
                             subtype2 = 1
                     elif (device["deviceURL"].startswith("rts://")):
+                        deviceType = 244
+                        subtype2 = 73
+                        used = 1 # 1 = True
                         swtype = 6
                     elif (device["definition"]["uiClass"] == "Pod"):
                         deviceType = 244
